636f9bb23468a985c803f98b17bc0812488258bf
formegusto <formegusto@gmail.com>
2021-05-14 15:11:22 +0900

    Init

---
+++ Component.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace house_uid_generator
+{
+	partial class Component
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.metroButton1 = new MetroFramework.Controls.MetroButton();
+			this.SuspendLayout();
+			// 
+			// metroButton1
+			// 
+			this.metroButton1.Location = new System.Drawing.Point(300, 150);
+			this.metroButton1.Name = "metroButton1";
+			this.metroButton1.Size = new System.Drawing.Size(200, 150);
+			this.metroButton1.TabIndex = 0;
+			this.metroButton1.Text = "Generate UID";
+			this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+			this.metroButton1.UseSelectable = true;
+			// 
+			// Component
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.metroButton1);
+			this.Name = "Component";
+			this.Text = "Household UID Generator";
+			this.Theme = MetroFramework.MetroThemeStyle.Dark;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private MetroFramework.Controls.MetroButton metroButton1;
+	}
+}
+
---
+++ Component.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace house_uid_generator
+{
+	public partial class Component : MetroFramework.Forms.MetroForm
+	{
+		public Component()
+		{
+			InitializeComponent();
+		}
+	}
+}
